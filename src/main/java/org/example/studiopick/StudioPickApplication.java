f9c7e0a6711ded7bd3bf2095b61806f5b40aec86
Fillsogood <jdd05273@naver.com>
2025-07-04 09:18:43 +0900

    feat: 초기 셋팅

---
+++ src/main/java/org/example/studiopick/StudioPickApplication.java
@@ -0,0 +1,13 @@
+package org.example.studiopick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioPickApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudioPickApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/studiopick/StudioPickApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studiopick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioPickApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
