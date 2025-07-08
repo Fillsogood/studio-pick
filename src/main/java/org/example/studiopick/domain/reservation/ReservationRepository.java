b29670c2a3dcc25df372cb71848a594c8c93aed5
camot0328 <camot0328@gmail.com>
2025-07-08 15:44:26 +0900

    Update ReservationRepository.java

--- src/main/java/org/example/studiopick/domain/reservation/ReservationRepository.java
+++ src/main/java/org/example/studiopick/domain/reservation/ReservationRepository.java
@@ -14,8 +14,6 @@ import java.util.Optional;
 public interface ReservationRepository {
   boolean existsOverlappingReservation(Long studioId, LocalDate date, ReservationStatus status, LocalTime start, LocalTime end);
 
-  Reservation save(Reservation reservation);
-
   List<Reservation> findByStudioIdAndReservationDateAndStatus(Long studioId, LocalDate reservationDate, ReservationStatus reservationStatus);
 
   Page<Reservation> findByUserIdOrderByReservationDateDesc(
