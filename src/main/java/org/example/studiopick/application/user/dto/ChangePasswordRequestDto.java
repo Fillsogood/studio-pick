db016d39606415513f6d8d0f7b8d1fdc7eee3bcc
unknown <bg072250@gmail.com>
2025-07-09 14:33:49 +0900

    비밀번호 변경 API

---
+++ src/main/java/org/example/studiopick/application/user/dto/ChangePasswordRequestDto.java
@@ -0,0 +1,16 @@
+package org.example.studiopick.application.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequestDto {
+
+    @NotBlank(message = "현재 비밀번호는 필수입니다.")
+    private String currentPassword;
+
+    @NotBlank(message = "새 비밀번호는 필수입니다.")
+    private String newPassword;
+}
