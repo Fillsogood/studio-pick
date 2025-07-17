a1ade23263184fb4b4e4109127e17b30c865de4d
unknown <bg072250@gmail.com>
2025-07-17 18:36:58 +0900

    refresh token

--- src/main/java/org/example/studiopick/application/auth/dto/UserLoginRequestDto.java
+++ src/main/java/org/example/studiopick/application/auth/dto/UserLoginRequestDto.java
@@ -17,4 +17,6 @@ public class UserLoginRequestDto {
     
     @NotBlank(message = "비밀번호는 필수입니다")
     private String password;
+
+    private boolean rememberMe;
 }
--- src/main/java/org/example/studiopick/config/WebConfig.java
+++ src/main/java/org/example/studiopick/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
+    registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000", "http://localhost:8000")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
